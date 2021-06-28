5266a4b850fef76973d3e7819a370d6799a18122
Son0-0 <jsmith032211@gmail.com>
2021-06-28 16:46:18 +0900

    #3 commit
    임시 주문 접수 화면 구성

--- app/src/main/java/com/example/owner/OrderList.java
+++ app/src/main/java/com/example/owner/OrderList.java
@@ -4,43 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.budiyev.android.codescanner.CodeScanner;
-import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.zxing.Result;
 
-public class Scan extends AppCompatActivity {
+public class OrderList extends AppCompatActivity {
+    private BottomNavigationView mBottomNV;
     private Toolbar toolbar;
-    private CodeScanner mCodeScanner;
-    private CodeScannerView mCodeScannerView;
-    private TextView resultTV;
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan);
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 123);
-        } else {
-            startScanning();
-        }
+        setContentView(R.layout.activity_orderlist);
 
         // toolbar setting
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -51,7 +35,15 @@ public class Scan extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
 
         //Set Home Selected
-        bottomNavigationView.setSelectedItemId(R.id.page_scan);
+        bottomNavigationView.setSelectedItemId(R.id.page_home);
+
+        // qrcode info
+        Intent intent = getIntent();
+        String menu = intent.getStringExtra("orderList");
+        Toast.makeText(this, menu, Toast.LENGTH_SHORT).show();
+
+        txt = (TextView) findViewById(R.id.orderList);
+        txt.setText(menu);
 
         //Perform ItemSelectedListener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -74,57 +66,14 @@ public class Scan extends AppCompatActivity {
                         finish();
                         return true;
                     case R.id.page_scan:
+                        startActivity(new Intent(getApplicationContext(), Scan.class));
+                        overridePendingTransition(R.anim.horizon_enter, R.anim.horizon_exit);
+                        finish();
                         return true;
                 }
                 return false;
             }
         });
-    }
-
-    private void startScanning() {
-        mCodeScannerView = findViewById(R.id.scanner_view);
-        mCodeScanner = new CodeScanner(this, mCodeScannerView);
-        mCodeScanner.startPreview(); // this linew is very important, as you will not be able to scan your code without this, you will only get black screen
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
-            @Override
-            public void onDecoded(@NonNull Result result) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent it = new Intent(getApplicationContext(), OrderList.class);
-                        it.putExtra("orderList", result.getText());
-
-                        resultTV = (TextView) findViewById(R.id.resultView);
-                        resultTV.setText(result.getText());
-
-                        startActivity(it);
-                        overridePendingTransition(R.anim.horizon_enter, R.anim.none);
-                        finish();
-                    }
-                });
-            }
-        });
-
-        // now if you want to scan again when you click on scanner then do this
-        mCodeScannerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCodeScanner.startPreview();
-            }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 123) {
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-                startScanning();
-            } else {
-                startScanning();
-            }
-        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
